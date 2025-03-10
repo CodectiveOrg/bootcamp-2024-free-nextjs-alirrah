37e1ca7e9f8df1192953eb1b505309b7bf22a97f
Alireza Rahmani Samani <alir.rah81@gmail.com>
2025-03-10 12:22:09 +0330

    [ADD] sign in page

--- src/app/auth/sign-in/page.tsx
+++ src/app/auth/sign-in/page.tsx
@@ -1,5 +1,24 @@
+import Link from "next/link";
+
+import CardComponent from "@/components/card/card.component";
+
+import SignInFormComponent from "@/app/auth/components/sign-in-form/sign-in-form.component";
+
 import styles from "./page.module.css";
 
 export default function Page() {
-  return <div className={styles.page}></div>;
+  return (
+    <CardComponent
+      outsideClassName={styles["outside-page"]}
+      className={styles.page}
+      showShadow
+    >
+      <h1>ورورد!</h1>
+      <SignInFormComponent />
+      <div className={styles["sign-up"]}>
+        <p> قبلاً ثبت‌نام نکردید؟</p>
+        <Link href="/auth/sign-up">ثبت‌نام کنید</Link>
+      </div>
+    </CardComponent>
+  );
 }

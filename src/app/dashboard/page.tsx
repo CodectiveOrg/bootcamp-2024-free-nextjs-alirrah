ff93e3f76ca7df6c3c5ef530df560c4cc00ab775
Alireza Rahmani Samani <alir.rah81@gmail.com>
2025-03-10 09:17:44 +0330

    [ADD] dashboard ui

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,12 @@
+import ButtonComponent from "@/components/button/button.component";
+
 import styles from "./page.module.css";
 
 export default function Page() {
-  return <div className={styles.page}></div>;
+  return (
+    <div className={styles.page}>
+      <h1>داشبورد</h1>
+      <ButtonComponent className={styles.exit}>خروج</ButtonComponent>
+    </div>
+  );
 }

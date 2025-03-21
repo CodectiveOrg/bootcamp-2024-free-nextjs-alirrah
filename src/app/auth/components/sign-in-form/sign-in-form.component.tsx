bc0d779608eb1c9ce6e298702e2f62e8da1f2041
alirrah <alir.rah81@gmail.com>
2025-03-21 18:57:45 +0330

    [ADD] sign in api

--- src/app/auth/components/sign-in-form/sign-in-form.component.tsx
+++ src/app/auth/components/sign-in-form/sign-in-form.component.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { FormEvent, ReactNode, useState } from "react";
+import { FormEvent, ReactNode, useRef, useState } from "react";
 
 import ButtonComponent from "@/components/button/button.component";
 import InputComponent from "@/components/input/input.component";
@@ -10,19 +10,47 @@ import MingcuteLockFill from "@/icon/MingcuteLockFill";
 import MingcuteEye2Line from "@/icon/MingcuteEye2Line";
 import MingcuteEyeCloseLine from "@/icon/MingcuteEyeCloseLine";
 
+import { fetchWithToast } from "@/utils/fetch.util";
+
+import { SignInDto } from "@/dto/auth.dto";
+
 import styles from "@/app/auth/styles/auth-form.module.css";
 
 export default function SignInFormComponent(): ReactNode {
+  const formRef = useRef<HTMLFormElement>(null);
+
   const [showPassword, setShowPassword] = useState(false);
 
   const formSubmitHandler = async (
     e: FormEvent<HTMLFormElement>,
   ): Promise<void> => {
     e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+
+    const dto: SignInDto = {
+      username: formData.get("username") as string,
+      password: formData.get("password") as string,
+    };
+
+    const result = await fetchWithToast<null>(
+      "/api/auth/sign-in",
+      {
+        method: "POST",
+        body: JSON.stringify(dto),
+      },
+      "خوش آمدید!",
+    );
+
+    if (result.error) {
+      return;
+    }
+
+    formRef.current?.reset();
   };
 
   return (
-    <form onSubmit={formSubmitHandler} className={styles.form}>
+    <form ref={formRef} onSubmit={formSubmitHandler} className={styles.form}>
       <InputComponent
         prefixIcon={<MingcuteUser3Fill />}
         placeholder="نام کاربری"

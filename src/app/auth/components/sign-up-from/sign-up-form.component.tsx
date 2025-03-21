c04c37093fe3f4d43c1106b0554a02d16275e90e
alirrah <alir.rah81@gmail.com>
2025-03-21 18:53:27 +0330

    [ADD] fetchWithToast

--- src/app/auth/components/sign-up-from/sign-up-form.component.tsx
+++ src/app/auth/components/sign-up-from/sign-up-form.component.tsx
@@ -1,8 +1,6 @@
 "use client";
 
-import { FormEvent, ReactNode, useState } from "react";
-
-import { toast } from "react-toastify";
+import { FormEvent, ReactNode, useRef, useState } from "react";
 
 import ButtonComponent from "@/components/button/button.component";
 import InputComponent from "@/components/input/input.component";
@@ -14,11 +12,15 @@ import MingcuteLockFill from "@/icon/MingcuteLockFill";
 import MingcuteEye2Line from "@/icon/MingcuteEye2Line";
 import MingcuteEyeCloseLine from "@/icon/MingcuteEyeCloseLine";
 
+import { fetchWithToast } from "@/utils/fetch.util";
+
 import { SignUpDto } from "@/dto/auth.dto";
 
 import styles from "@/app/auth/styles/auth-form.module.css";
 
 export default function SignUpFormComponent(): ReactNode {
+  const formRef = useRef<HTMLFormElement>(null);
+
   const [showPassword, setShowPassword] = useState(false);
 
   const formSubmitHandler = async (
@@ -35,29 +37,24 @@ export default function SignUpFormComponent(): ReactNode {
       password: formData.get("password") as string,
     };
 
-    const response = await fetch("/api/auth/sign-up", {
-      method: "POST",
-      body: JSON.stringify(dto),
-    });
+    const result = await fetchWithToast<null>(
+      "/api/auth/sign-up",
+      {
+        method: "POST",
+        body: JSON.stringify(dto),
+      },
+      "ثبت‌نام با موفقیت انجام شد.",
+    );
 
-    const result = await response.json();
-
-    if (!response.ok) {
-      let message: string = "خطای غیرمنتظره رخ داد.";
-
-      if ("error" in result) {
-        message = result.error;
-      }
-
-      toast.error(message);
+    if (result.error) {
       return;
     }
 
-    toast.success("ثبت‌نام با موفقیت انجام شد.");
+    formRef.current?.reset();
   };
 
   return (
-    <form onSubmit={formSubmitHandler} className={styles.form}>
+    <form ref={formRef} onSubmit={formSubmitHandler} className={styles.form}>
       <InputComponent
         prefixIcon={<MingcuteIdcardFill />}
         name="name"
